d020309efbdf42e45ccd5fa0cd8b7290c696bb75
f1fe <rob@onequarterninja.com>
2015-09-26 16:06:39 -0400

    show and hide the descriptions

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,19 @@
 console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
 $(function() {
   $( '#draggable' ).draggable;
-  $( "#draggable" ).resizable();
+  // $( "#draggable" ).resizable();
 });
+
+function showDescription(elementid) {
+  console.log("show " + elementid);
+  var allids = ["#hiddenText1", "#hiddenText2", "#hiddenText3", "#hiddenText4",
+                "#hiddenText5", "#hiddenText6", "#hiddenText7", "#hiddenText8", "#hiddenText9"];
+  for (var i = 0; i < allids.length; i++) {
+    var e = $(allids[i]);
+    e.hide();
+  }
+
+  //show the selected one
+  var selectableName = "#"+elementid;
+  $(selectableName).show();
+}
